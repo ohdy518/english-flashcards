0427d965179a9210081a8be6c9d0ecb295783031
ohdy518 <ohdy518@gmail.com>
2025-06-06 12:09:16 +0900

    Initial commit II

---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Content from "./Content.svelte";
+
+    let defSet = {word: "egotism", fos: "n.", def: "thinking only about yourself and considering yourself better and more important than other people."}
+
+    let userInput;
+
+    let correct = 0;
+
+    function check() {
+        document.getElementById("userInput").disabled = true;
+        if (userInput.trim() === defSet.word) {
+            correct = 1;
+        } else {correct = -1}
+    }
+
+    function newWord() {
+        console.log("new!")
+    }
+
+    function userSubmit() {
+        if (correct === 0) {
+            check()
+        } else {
+            newWord()
+        }
+    }
+</script>
+
+<div class="h-screen w-screen inter bg-zinc-200 text-zinc-950 flex flex-col items-center justify-center">
+    <Content defSet={defSet} hide={correct === 0} correct={correct}/>
+
+    <div id="inputs" class="mt-12
+                            w-80 sm:w-96
+                            flex flex-row items-center justify-center">
+        <input id="userInput" bind:value={userInput} class="bg-zinc-50 h-8 sm:h-10 w-48 sm:w-72 border-2 border-zinc-600 px-2 mr-2 jbm text-base" placeholder="enter word..."/>
+        <button id="check" class="bg-zinc-50 px-1.5 h-full border-2 border-zinc-600" on:click={userSubmit}>-> {correct===0?"Check":"Next"}</button>
+    </div>
+</div>
---
+++ src/routes/play/Content.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let defSet;
+    export let hide;
+    export let correct;
+</script>
+
+<div class="bg-zinc-50
+            w-80 sm:w-120 h-48 sm:h-72
+            text-base sm:text-lg
+            border-3 {correct===0?'border-zinc-950':(correct===1?'border-green-500':'border-rose-500')} rounded-lg flex flex-col items-center justify-center ssr">
+    <span class="font-bold text-lg sm:text-2xl pb-6 {hide?'hidden':'visible'}">egotism</span>
+    <div class="max-w-64 sm:max-w-96">
+        <span class="italic rounded-lg bg-zinc-200 text-zinc-700 px-1.5">{defSet.fos}</span>
+        <span class="font-semibold text-balance">{defSet.def}</span>
+    </div>
+</div>
