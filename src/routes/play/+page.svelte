04f1e8d18e5b8644fe21bb9073dbf9d2e93b320d
ohdy518 <ohdy518@gmail.com>
2026-03-23 13:10:44 +0900

    Update.

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import Content from "./Content.svelte";
-    import {words} from "$lib/wordlist5.js";
+    import {words} from "$lib/wordlist6.js";
     import {browser} from "$app/environment";
 
     let totalLen = words.length;
