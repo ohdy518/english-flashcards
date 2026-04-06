d8533d28610836402ce9bea1ea927ebb24ae3ec3
ohdy518 <ohdy518@gmail.com>
2026-04-07 00:51:50 +0900

    Update.

---
+++ src/lib/ww7c7.js
@@ -0,0 +1,17 @@
+export const words = [
+    {word: "beseech", fos: "v.", def: "To ask earnestly; to beg."},
+    {word: "consternation", fos: "n.", def: "Amazement or fear that makes one feel confused."},
+    {word: "delectable", fos: "adj.", def: "Pleasing to the senses, especially to the sense of taste; delicious."},
+    {word: "garland", fos: "n.", def: "A wreath or chain of leaves and flowers."},
+    {word: "gratify", fos: "v.", def: "To please or satisfy."},
+    {word: "haughty", fos: "adj.", def: "Showing too much pride in oneself and scorn or contempt for others."},
+    {word: "impetuous", fos: "adj.", def: "Likely to act without thinking; hasty."},
+    {word: "lavish", fos: "adj.", def: "Much mure than enough."},
+    {word: "pluck", fos: "v.", def: "To pull off or out; to pick."},
+    {word: "ponder", fos: "v.", def: "To think about; to consider carefully."},
+    {word: "privilege", fos: "n.", def: "A special favor, right, or advantage given to a person or group."},
+    {word: "prostrate", fos: "adj.", def: "Lying flat."},
+    {word: "rapture", fos: "n.", def: "A state of great joy, delight, or love."},
+    {word: "revelry", fos: "n.", def: "Noisy merrymaking."},
+    {word: "whim", fos: "n.", def: "A sudden wish to do something without a particular reason; a fanciful idea."}
+];
